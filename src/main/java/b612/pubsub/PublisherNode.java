b2e78e4cac9f0a9b9ab34c935a1de1be753c5aaa
Mahesh M <maheshmukundan@gmail.com>
2019-08-11 22:21:46 +0200

    Implement message loop back and job wait

--- src/main/java/b612/pubsub/PublisherNode.java
+++ src/main/java/b612/pubsub/PublisherNode.java
@@ -2,61 +2,77 @@ package b612.pubsub;
 
 import com.google.api.core.ApiFuture;
 import com.google.api.core.ApiFutures;
-import com.google.cloud.ServiceOptions;
 import com.google.cloud.pubsub.v1.Publisher;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.ProjectTopicName;
 import com.google.pubsub.v1.PubsubMessage;
 
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.v1.MessageReceiver;
+import com.google.cloud.pubsub.v1.Subscriber;
+import com.google.pubsub.v1.ProjectSubscriptionName;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class PublisherNode {
 
-  // use the default project id
-  private static final String PROJECT_ID = ServiceOptions.getDefaultProjectId();
-
-  /** Publish messages to a topic.
-   * @param args topic name, number of messages
-   */
-  public static void main(String... args) throws Exception {
-    // topic id, eg. "my-topic"
-    String topicId = "my-topic"; //args[0];
-    int messageCount = 10; //Integer.parseInt(args[1]);
-    ProjectTopicName topicName = ProjectTopicName.of("weighty-vertex-165601", topicId);
-    Publisher publisher = null;
-    List<ApiFuture<String>> futures = new ArrayList<>();
-
-    try {
-      // Create a publisher instance with default settings bound to the topic
-      publisher = Publisher.newBuilder(topicName).build();
-
-      for (int i = 0; i < messageCount; i++) {
-        String message = "message-" + i;
-
-        // convert message to bytes
-        ByteString data = ByteString.copyFromUtf8(message);
-        PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
-            .setData(data)
-            .build();
-
-        // Schedule a message to be published. Messages are automatically batched.
-        ApiFuture<String> future = publisher.publish(pubsubMessage);
-        futures.add(future);
-      }
-    } finally {
-      // Wait on any pending requests
-      List<String> messageIds = ApiFutures.allAsList(futures).get();
-
-      for (String messageId : messageIds) {
-        System.out.println(messageId);
-      }
-
-      if (publisher != null) {
-        // When finished with the publisher, shutdown to free up resources.
-        publisher.shutdown();
-      }
+    static class MessageReceiverExample implements MessageReceiver {
+        @Override
+        public void receiveMessage(PubsubMessage message, AckReplyConsumer consumer) {
+            System.out.println("PubNode: Recieved Message");
+            System.out.println("Data: " + message.getData().toStringUtf8());
+            consumer.ack();
+        }
     }
-  }
+
+    public static void main(String... args) throws Exception {
+
+        String subscriptionId = "my-topic-resp";
+        ProjectSubscriptionName subscriptionName = ProjectSubscriptionName.of("weighty-vertex-165601", subscriptionId);
+        Subscriber subscriber = null;
+        try {
+            subscriber = Subscriber.newBuilder(subscriptionName, new MessageReceiverExample()).build();
+            subscriber.startAsync().awaitRunning();
+            System.out.println("PublisherNode: subscriber created");
+        } catch (IllegalStateException e) {
+            System.out.println("PublisherNode: Subscriber unexpectedly stopped: " + e);
+        }
+
+        System.out.println("PublisherNode: creating pub");
+        String topicId = "my-topic";
+
+        int messageCount = 10;
+        ProjectTopicName topicName = ProjectTopicName.of("weighty-vertex-165601", topicId);
+        Publisher publisher = null;
+        List<ApiFuture<String>> futures = new ArrayList<>();
+
+        try {
+            publisher = Publisher.newBuilder(topicName).build();
+            System.out.println("PublisherNode: created. sending data...");
+            for (int i = 0; i < messageCount; i++) {
+                String message = "message-" + i;
+
+                ByteString data = ByteString.copyFromUtf8(message);
+                PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
+                    .setData(data)
+                    .build();
+
+                ApiFuture<String> future = publisher.publish(pubsubMessage);
+                futures.add(future);
+                System.out.println("PublisherNode: sent data "+ i);
+            }
+        } finally {
+            List<String> messageIds = ApiFutures.allAsList(futures).get();
+
+            for (String messageId : messageIds) {
+                System.out.println(messageId);
+            }
+
+            if (publisher != null) {
+                publisher.shutdown();
+                subscriber.awaitTerminated();
+            }
+        }
+    }
 }
--- src/main/java/b612/pubsub/SubscriberNode.java
+++ src/main/java/b612/pubsub/SubscriberNode.java
@@ -1,43 +1,76 @@
 package b612.pubsub;
 
-import com.google.cloud.ServiceOptions;
 import com.google.cloud.pubsub.v1.AckReplyConsumer;
 import com.google.cloud.pubsub.v1.MessageReceiver;
 import com.google.cloud.pubsub.v1.Subscriber;
 import com.google.pubsub.v1.ProjectSubscriptionName;
 import com.google.pubsub.v1.PubsubMessage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.pubsub.v1.Publisher;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.ProjectTopicName;
+
 public class SubscriberNode {
-  // use the default project id
-  private static final String PROJECT_ID = ServiceOptions.getDefaultProjectId();
+    private static Publisher publisher = null;
 
-  static class MessageReceiverExample implements MessageReceiver {
+    static class MessageReceiverExample implements MessageReceiver {
+        @Override
+        public void receiveMessage(PubsubMessage message, AckReplyConsumer consumer) {
+            System.out.println("SubscriberNode: Recieved message");
+            System.out.println("SubcriberNode: Data: " + message.getData().toStringUtf8());
+            try {
+                System.out.println("SubscriberNode: Doing the heavylifting of sleeping!");
+                int random = (int)(Math.random() * 1000);
+                Thread.sleep(random);
+                System.out.println("SubscriberNode: Done with the heavylifting of sleeping!");
 
-    @Override
-    public void receiveMessage(PubsubMessage message, AckReplyConsumer consumer) {
-      System.out.println(
-          "Message Id: " + message.getMessageId() + " Data: " + message.getData().toStringUtf8());
-      // Ack only after all work for the message is complete.
-      consumer.ack();
+                acknowledge(message.getData());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+            consumer.ack();
+        }
     }
-  }
 
-  /** Receive messages over a subscription. */
-  public static void main(String... args) throws Exception {
-    // set subscriber id, eg. my-sub
-    String subscriptionId = "my-topic";//args[0];
-    ProjectSubscriptionName subscriptionName = ProjectSubscriptionName.of(
-        "weighty-vertex-165601", subscriptionId);
-    Subscriber subscriber = null;
-    try {
-      // create a subscriber bound to the asynchronous message receiver
-      subscriber =
-          Subscriber.newBuilder(subscriptionName, new MessageReceiverExample()).build();
-      subscriber.startAsync().awaitRunning();
-      // Allow the subscriber to run indefinitely unless an unrecoverable error occurs.
-      subscriber.awaitTerminated();
-    } catch (IllegalStateException e) {
-      System.out.println("Subscriber unexpectedly stopped: " + e);
+    public static void acknowledge(ByteString message) throws InterruptedException, ExecutionException {
+        List<ApiFuture<String>> futures = new ArrayList<>();
+
+        try {
+            PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
+                .setData(message)
+                .build();
+            System.out.println("SubsciberNode: trying to ack message: "+ message);
+            ApiFuture<String> future = SubscriberNode.publisher.publish(pubsubMessage);
+            futures.add(future);
+        } finally {
+        	System.out.println("SubscriberNode: Hopefully acked");
+        }
     }
-  }
+
+    public static void main(String... args) throws Exception {
+        String topicId = "my-topic-resp";
+        ProjectTopicName topicName = ProjectTopicName.of("weighty-vertex-165601", topicId);
+
+        SubscriberNode.publisher = Publisher.newBuilder(topicName).build();
+        System.out.println("SubscriberNode: built publisher");
+
+        String subscriptionId = "my-topic";
+        ProjectSubscriptionName subscriptionName = ProjectSubscriptionName.of("weighty-vertex-165601", subscriptionId);
+        Subscriber subscriber = null;
+        try {
+            subscriber = Subscriber.newBuilder(subscriptionName, new MessageReceiverExample()).build();
+            subscriber.startAsync().awaitRunning();
+            System.out.println("SubscriberNode: sub waiting for data");
+
+            subscriber.awaitTerminated();
+            System.out.println("SubscriberNode: sub waiting for termination");
+        } catch (IllegalStateException e) {
+            System.out.println("Subscriber unexpectedly stopped: " + e);
+        }
+    }
 }
